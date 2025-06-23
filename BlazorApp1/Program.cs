71caa635383701d9574f6357ce95e9f44bbfef90
R0b3rtG <robertiancu24@gmail.com>
2025-06-23 11:17:24 +0300

    Starting point

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,10 +1,22 @@
 using BlazorApp1.Components;
 
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 var app = builder.Build();
 
