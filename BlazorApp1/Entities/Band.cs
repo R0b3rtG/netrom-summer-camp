f343bb0abb2db3b23650d89cbceeecea56dabdf0
R0b3rtG <robertiancu24@gmail.com>
2025-07-01 19:25:20 +0300

    created pages for adding and listing bands

--- BlazorApp1/Entities/Band.cs
+++ BlazorApp1/Entities/Band.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.VisualBasic;
 using BlazorApp1.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorApp1.Entities
 {
     public class Band
     {
-        public Guid Id { get; set; }
+		public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public Genre Genre { get; set; }
         public ICollection<Festival> Festivals { get; set; } = [];
